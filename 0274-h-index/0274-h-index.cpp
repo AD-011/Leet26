cbe5de285b21dee90d0cdfb3f856ce9d381c7333
Akash Das <akash11745@gmail.com>
2026-08-25 23:52:05 +0530

    Time: 0 ms (100%), Space: 12.2 MB (63.68%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        vector<int> count(n + 1, 0);
-
-        for (int c : citations) {
-            if (c >= n) {
-                count[n]++;
-            } else {
-                count[c]++;
-            }
-        }
-
-        int total = 0;
-        for (int i = n; i >= 0; --i) {
-            total += count[i];
-            if (total >= i) {
-                return i;
-            }
-        }
-
+  int n = citations.size();
+        sort(citations.begin(), citations.end());
+        for ( int i =0; i< n;i++)
+        {
+           if( citations[i] >= n-i)
+           {
+            return n-i;
+           }
+            
+        } 
         return 0;
-        
     }
 };
